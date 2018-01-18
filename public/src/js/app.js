7b2f2027a166fe005f470e6bb9f50dd6f5774813
Yoyae <gingin.fr@gmail.com>
2018-01-18 09:52:37 +0100

    Monoecify

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var defaultLanguage = localStorage.getItem('insight-language') || 'en';
-var defaultCurrency = localStorage.getItem('insight-currency') || 'Monoeci';
+var defaultCurrency = localStorage.getItem('insight-currency') || 'XMCC';
 
 angular.module('insight',[
   'ngAnimate',
